07a86ceaafddb9fc1092a60daa1aa324bea18f29
Praga24 <pragadeeshbas@gmail.com>
2017-04-05 18:41:12 +0530

    Add files via upload

--- Assign.java
+++ Assign.java
@@ -1,8 +1,8 @@
-import org.apache.hadoop.fs.Path; 
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat; 
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat; 
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
@@ -10,33 +10,32 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 
 public class Assign {
-	@SuppressWarnings("deprecation")
-	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
-		Job job = new Job(conf, "Demo");
-		job.setJarByClass(Assign.class);
+@SuppressWarnings("deprecation")
+public static void main(String[] args) throws Exception {
+Configuration conf = new Configuration();
+Job job = new Job(conf, "Demo");
+job.setJarByClass(Assign.class);
 
-		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(IntWritable.class);
+job.setMapOutputKeyClass(Text.class);
+job.setMapOutputValueClass(IntWritable.class);
 
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
-		job.setMapperClass(AssignMapper.class);
-		job.setNumReduceTasks(0);
-		
-		 
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(SequenceFileOutputFormat.class);
+job.setOutputKeyClass(Text.class);
+job.setOutputValueClass(IntWritable.class);
+job.setMapperClass(AssignMapper.class);
+job.setNumReduceTasks(0);
 
-		FileInputFormat.addInputPath(job, new Path(args[0])); 
-		FileOutputFormat.setOutputPath(job,new Path(args[1]));
-		
-		/*
-		Path out=new Path(args[1]);
-		out.getFileSystem(conf).delete(out);
-		*/
-		
-		job.waitForCompletion(true);
-	}
+
+job.setInputFormatClass(TextInputFormat.class);
+job.setOutputFormatClass(SequenceFileOutputFormat.class);
+
+FileInputFormat.addInputPath(job, new Path(args[0]));
+FileOutputFormat.setOutputPath(job,new Path(args[1]));
+
+/*
+Path out=new Path(args[1]);
+out.getFileSystem(conf).delete(out);
+*/
+
+job.waitForCompletion(true);
 }
-
+}
